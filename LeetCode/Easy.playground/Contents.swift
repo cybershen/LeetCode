33c97aa7a82a94f72cc0a68efbad28260b866489
cybershen <cyberpuer@proton.me>
2023-05-15 09:52:01 +0300

    Solve Task 125. Valid Palindrome and Task 27. Remove Element

--- LeetCode/Easy.playground/Contents.swift
+++ LeetCode/Easy.playground/Contents.swift
@@ -322,3 +322,114 @@ let str2 = "luffy is still joyboy"
 
 lengthOfLastWord(str)
 lengthOfLastWord(str2)
+
+//MARK: - Task 125. Valid Palindrome
+
+/*
+ A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+ Given a string s, return true if it is a palindrome, or false otherwise.
+
+ Example 1:
+
+ Input: s = "A man, a plan, a canal: Panama"
+ Output: true
+ Explanation: "amanaplanacanalpanama" is a palindrome.
+ 
+ Example 2:
+
+ Input: s = "race a car"
+ Output: false
+ Explanation: "raceacar" is not a palindrome.
+ 
+ Example 3:
+
+ Input: s = " "
+ Output: true
+ Explanation: s is an empty string "" after removing non-alphanumeric characters.
+ Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+ */
+
+func isPalindrome(_ s: String) -> Bool {
+    var arrayOfChars: [Character] = []
+    
+    for char in s {
+        if char.isLetter || char.isNumber {
+            arrayOfChars.append(Character(String(char.lowercased())))
+        }
+    }
+    
+    return arrayOfChars == arrayOfChars.reversed()
+}
+
+//MARK: - Task 27. Remove Element
+
+/*
+ 
+ Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+ Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+ Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+ Return k.
+ Custom Judge:
+
+ The judge will test your solution with the following code:
+ 
+ int[] nums = [...]; // Input array
+ int val = ...; // Value to remove
+ int[] expectedNums = [...]; // The expected answer with correct length.
+                             // It is sorted with no values equaling val.
+
+ int k = removeElement(nums, val); // Calls your implementation
+
+ assert k == expectedNums.length;
+ sort(nums, 0, k); // Sort the first k elements of nums
+ for (int i = 0; i < actualLength; i++) {
+     assert nums[i] == expectedNums[i];
+ }
+ 
+ If all assertions pass, then your solution will be accepted.
+
+  
+
+ Example 1:
+
+ Input: nums = [3,2,2,3], val = 3
+ Output: 2, nums = [2,2,_,_]
+ Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+ Example 2:
+
+ Input: nums = [0,1,2,2,3,0,4,2], val = 2
+ Output: 5, nums = [0,1,4,0,3,_,_,_]
+ 
+ Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+ Note that the five elements can be returned in any order.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+ */
+
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    let count = nums.count
+    
+    nums.removeAll { num in
+        num == val
+    }
+    
+    let resultCount = nums.count
+    
+    let newCount = count - nums.count
+    
+    for _ in 0...max(newCount - 1, 0) {
+        nums.append(val)
+    }
+    
+    return resultCount
+}
+
+var array55 = [55, 2432, 34, 523, 2]
+
+removeElement(&array55, 0)
